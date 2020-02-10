e9a638618e885a38890b4ac0c4527f2a9938e763
IngJulioPerez <60108127+IngJulioPerez@users.noreply.github.com>
2020-02-10 18:30:08 -0400

    Entidad Articulo

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RegistroPrimerParcial.Entidades;
+
+
+namespace RegistroPrimerParcial.DAL
+{
+    class Contexto
+    {
+    }
+}
---
+++ Entidades/Articulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace RegistroPrimerParcial.Entidades
+{
+    public class Articulo
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public int Costo { get; set; }
+        public int ValorInventario { get; set; }
+    }
+}
